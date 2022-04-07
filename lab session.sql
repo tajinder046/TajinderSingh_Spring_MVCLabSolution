93ade307858318e5b45b5cb593947705a3a3a866
tajinder046 <95207787+tajinder046@users.noreply.github.com>
2022-04-07 22:41:42 +0530

    file upload

---
+++ lab session.sql
@@ -0,0 +1,10 @@
+create database studentdatabase;
+
+use studentdatabase;
+
+create table student (
+id int primary key auto_increment,
+name varchar(50),
+department varchar(50),
+country varchar(50)
+);
